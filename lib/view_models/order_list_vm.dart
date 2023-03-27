e7fef56901c70146c4145cfeffb5f948da73ec5a
Shubham Sen <shubham.s@ibirdsservices.com>
2023-03-27 13:26:04 +0530

    changes

--- lib/view_models/order_list_vm.dart
+++ lib/view_models/order_list_vm.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:study_evaluation/core/models/base_list_view_model.dart';
 import 'package:study_evaluation/core/models/order_response_model.dart';
@@ -19,6 +20,8 @@ class OrderListViewModel extends BaseListViewModel {
   }
 
   Future<dynamic> placeOrder(String packageId, String amount) async {
+    String currentDate = DateFormat('yyyy-MM-dd').format(DateTime.now());
+
     SharedPreferences prefs = await SharedPreferences.getInstance();
     UserModel userModel = AppUtils.getSessionUser(prefs);
     String url = AppUtils.getUrl(AppConstants.orderPaymentCreationAPIPath);
@@ -27,7 +30,7 @@ class OrderListViewModel extends BaseListViewModel {
       'package_id': packageId,
       'order_number': '2332323',
       'transaction_id': '34345353',
-      'order_date': '17-03-2023',
+      'order_date': currentDate,
       'amount': amount,
       'payment_status': 'Confirmed',
       'payment_type': 'Online',
