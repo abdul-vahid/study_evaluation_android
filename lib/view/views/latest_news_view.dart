8edfe0da579b2269f47619b2a87163ccafd3fad3
Shubham Sen <shubham.s@ibirdsservices.com>
2023-04-07 15:51:21 +0530

    changes

--- lib/view/views/latest_news_view.dart
+++ lib/view/views/latest_news_view.dart
@@ -74,10 +74,7 @@ class _LatestNewsViewState extends State<LatestNewsView> {
     return SingleChildScrollView(
         child: Column(
       children: [
-        _getCurrentAffairVideoWidget(),
-
-        //getTextContainer('Motivational Video'),
-
+        _gelatestNewsVideoWidget(),
         const SizedBox(
           height: 10,
         ),
@@ -85,11 +82,11 @@ class _LatestNewsViewState extends State<LatestNewsView> {
     ));
   }
 
-  Container _getCurrentAffairVideoWidget() {
-    return Container(child: Column(children: _getCurrentAffairVideoWidgets));
+  Container _gelatestNewsVideoWidget() {
+    return Container(child: Column(children: _getLatestNewsVideoWidgets));
   }
 
-  List<Widget> get _getCurrentAffairVideoWidgets {
+  List<Widget> get _getLatestNewsVideoWidgets {
     List<Widget> widgets = [];
     baseListViewModel?.latestNewsData.forEach((key, viewModels) {
       List<Widget> videoBottomSheetWidgets = [];
@@ -135,14 +132,6 @@ class _LatestNewsViewState extends State<LatestNewsView> {
           TextButton.icon(
               onPressed: () async {
                 AppUtils.openDocument(context, latestNewsModel?.pdfUrl);
-                // final url =
-                //     '${AppConstants.baseUrl}${AppConstants.baseUrl}/${latestNewsModel?.pdfUrl}';
-                // final uri = Uri.parse(url);
-                // if (await canLaunchUrl(uri)) {
-                //   await launchUrl(uri);
-                // } else {
-                //   throw 'Could not launch $url';
-                // }
               },
               icon: const Icon(
                 Icons.picture_as_pdf,
@@ -159,11 +148,6 @@ class _LatestNewsViewState extends State<LatestNewsView> {
                         '${AppConstants.baseUrl}${AppConstants.publicPath}/${latestNewsModel?.documentUrl}',
                         subject: 'Welcome Message')
                   },
-              // onPressed: () => {
-              //       Share.share(
-              //           '${AppConstants.baseUrl}${AppConstants.baseUrl}/${latestNewsModel?.documentUrl}',
-              //           subject: 'Welcome Message')
-              //     },
               icon: const Icon(
                 Icons.share,
                 color: Colors.black,
@@ -270,179 +254,3 @@ class _LatestNewsViewState extends State<LatestNewsView> {
             "${AppConstants.baseUrl}${AppConstants.publicPath}/$videoUrl"));
   }
 }
-
-// class _VideoPlayer extends StatefulWidget {
-//   String videoUrl;
-//   _VideoPlayer(this.videoUrl);
-
-//   @override
-//   _VideoPlayerState createState() => _VideoPlayerState(videoUrl);
-// }
-
-// class _VideoPlayerState extends State<_VideoPlayer> {
-//   String videoUrl;
-//   _VideoPlayerState(this.videoUrl);
-//   late VideoPlayerController _controller;
-
-//   @override
-//   void initState() {
-//     super.initState();
-
-//     _controller = VideoPlayerController.network(
-//         AppConstants.baseUrl + AppConstants.publicPath + '/' + videoUrl);
-
-//     _controller.addListener(() {
-//       setState(() {});
-//     });
-//     _controller.setLooping(true);
-//     _controller.initialize().then((_) => setState(() {}));
-//     _controller.pause();
-//   }
-
-//   @override
-//   void dispose() {
-//     _controller.dispose();
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return SingleChildScrollView(
-//       child: Column(
-//         children: <Widget>[
-//           // Container(
-//           //   padding: const EdgeInsets.only(top: 20.0),
-//           // ),
-//           AspectRatio(
-//             aspectRatio: _controller.value.aspectRatio,
-//             child: Stack(
-//               alignment: Alignment.bottomCenter,
-//               children: <Widget>[
-//                 VideoPlayer(_controller),
-//                 _ControlsOverlay(controller: _controller),
-//                 VideoProgressIndicator(
-//                   _controller,
-//                   allowScrubbing: true,
-//                 ),
-//               ],
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-// class _ControlsOverlay extends StatelessWidget {
-//   const _ControlsOverlay({Key? key, required this.controller})
-//       : super(key: key);
-
-//   static const List<Duration> _exampleCaptionOffsets = <Duration>[
-//     Duration(seconds: -10),
-//     Duration(seconds: -3),
-//     Duration(seconds: -1, milliseconds: -500),
-//     Duration(milliseconds: -250),
-//     Duration.zero,
-//     Duration(milliseconds: 250),
-//     Duration(seconds: 1, milliseconds: 500),
-//     Duration(seconds: 3),
-//     Duration(seconds: 10),
-//   ];
-//   static const List<double> _examplePlaybackRates = <double>[
-//     0.25,
-//     0.5,
-//     1.0,
-//     1.5,
-//     2.0,
-//     3.0,
-//     5.0,
-//     10.0,
-//   ];
-
-//   final VideoPlayerController controller;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Stack(
-//       children: <Widget>[
-//         AnimatedSwitcher(
-//           duration: const Duration(milliseconds: 50),
-//           reverseDuration: const Duration(milliseconds: 200),
-//           child: controller.value.isPlaying
-//               ? const SizedBox.shrink()
-//               : Container(
-//                   color: Colors.black26,
-//                   child: const Center(
-//                     child: Icon(
-//                       Icons.play_arrow,
-//                       color: Colors.white,
-//                       size: 100.0,
-//                       semanticLabel: 'Play',
-//                     ),
-//                   ),
-//                 ),
-//         ),
-//         GestureDetector(
-//           onTap: () {
-//             controller.value.isPlaying ? controller.pause() : controller.play();
-//           },
-//         ),
-//         Align(
-//           alignment: Alignment.topLeft,
-//           child: PopupMenuButton<Duration>(
-//             initialValue: controller.value.captionOffset,
-//             tooltip: 'Caption Offset',
-//             onSelected: (Duration delay) {
-//               controller.setCaptionOffset(delay);
-//             },
-//             itemBuilder: (BuildContext context) {
-//               return <PopupMenuItem<Duration>>[
-//                 for (final Duration offsetDuration in _exampleCaptionOffsets)
-//                   PopupMenuItem<Duration>(
-//                     value: offsetDuration,
-//                     child: Text('${offsetDuration.inMilliseconds}ms'),
-//                   )
-//               ];
-//             },
-//             child: Padding(
-//               padding: const EdgeInsets.symmetric(
-//                 // Using less vertical padding as the text is also longer
-//                 // horizontally, so it feels like it would need more spacing
-//                 // horizontally (matching the aspect ratio of the video).
-//                 vertical: 12,
-//                 horizontal: 16,
-//               ),
-//               child: Text('${controller.value.captionOffset.inMilliseconds}ms'),
-//             ),
-//           ),
-//         ),
-//         Align(
-//           alignment: Alignment.topRight,
-//           child: PopupMenuButton<double>(
-//             initialValue: controller.value.playbackSpeed,
-//             tooltip: 'Playback speed',
-//             onSelected: (double speed) {
-//               controller.setPlaybackSpeed(speed);
-//             },
-//             itemBuilder: (BuildContext context) {
-//               return <PopupMenuItem<double>>[
-//                 for (final double speed in _examplePlaybackRates)
-//                   PopupMenuItem<double>(
-//                     value: speed,
-//                     child: Text('${speed}x'),
-//                   )
-//               ];
-//             },
-//             child: Padding(
-//               padding: const EdgeInsets.symmetric(
-//                 vertical: 12,
-//                 horizontal: 16,
-//               ),
-//               child: Text('${controller.value.playbackSpeed}'),
-//             ),
-//           ),
-//         ),
-//       ],
-//     );
-//   }
-// }
